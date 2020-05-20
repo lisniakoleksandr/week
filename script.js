8ffb6e180ae01899886f254f522c987337a3c812
Oleksandr Lisniak <65028714+lisniakoleksandr@users.noreply.github.com>
2020-05-20 10:18:09 +0300

    forc check
    *Создайте объект с днями недели. Ключами в нем должны служить номера дней от начала недели
     (понедельник - первый и т.д.). Выведите на экран текущий день недели. */

---
+++ script.js
@@ -0,0 +1,23 @@
+/*Создайте объект с днями недели. Ключами в нем должны служить номера дней от начала недели
+ (понедельник - первый и т.д.). Выведите на экран текущий день недели. */
+
+ var week = {
+
+ 1: 'monday',
+ 2: 'tuesday',
+  3: 'wednesday' ,
+  4: 'thursday',
+ 5:  'friday',
+ 6: 'saturday' ,
+ 7:  'sunday' ,
+
+
+ };
+
+var day  = prompt ('enter the day number','' );
+alert(week[day])
+
+     
+ 
+
+  
